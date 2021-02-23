5ab9d7e47ca9157ea52d4e0b12181358a8f91560
galaxy1 <st989@ya.ru>
2021-02-23 19:02:38 +0400

    Initial commit

---
+++ app/src/main/java/com/pipe/geekbrains_my_app_weather/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pipe.geekbrains_my_app_weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
