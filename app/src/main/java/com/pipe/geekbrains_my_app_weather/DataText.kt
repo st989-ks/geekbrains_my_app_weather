662a363236b1ea33e0db31126432a14934eac453
galaxy1 <st989@ya.ru>
2021-02-24 21:11:54 +0400

    Первое задание готово

---
+++ app/src/main/java/com/pipe/geekbrains_my_app_weather/DataText.kt
@@ -0,0 +1,6 @@
+package com.pipe.geekbrains_my_app_weather
+
+class DataText
+const val TAG_FIRST = "TAG_FIRST"
+const val TAG_SECOND = "TAG_SECOND"
+const val TAG_THIRD = "TAG_THIRD"
--- app/src/main/java/com/pipe/geekbrains_my_app_weather/MainActivity.kt
+++ app/src/main/java/com/pipe/geekbrains_my_app_weather/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.pipe.geekbrains_my_app_weather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        var first: MyClass = MyClass("", 1)
+        val button: Button = findViewById(R.id.test_button)
+        var etText = findViewById<EditText>(R.id.text_enter)
+        var tvText = findViewById<TextView>(R.id.text_view)
+
+        button.setOnClickListener {
+            val second: MyClass = first.copy(text = etText.text.toString())
+            tvText.text = etText.text
+            Toast.makeText(this@MainActivity, "${second.text}, ${second.number}", Toast.LENGTH_LONG).show()
+            cycle()
+        }
+    }
+    
+    fun cycle (){
+        val list = listOf(1, 2, 1, 4, 22)
+        list.forEach {
+            Log.d(TAG_FIRST, it.toString())
+        }
+        for (i in 1..50 step 2) {
+            Log.d(TAG_SECOND, i.toString())
+        }
+        for (i in 20 downTo 1 step 3) {
+            Log.d(TAG_THIRD, i.toString())
+        }
     }
 }
