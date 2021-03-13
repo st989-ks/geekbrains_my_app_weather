618bb98f09bbeaf28ec4578998cf700c4eb89dcc
galaxy1 <st989@ya.ru>
2021-03-13 19:44:44 +0400

    #5

--- app/src/main/java/com/pipe/geekbrains_my_app_weather/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/pipe/geekbrains_my_app_weather/viewmodel/MainViewModel.kt
@@ -23,7 +23,7 @@ class MainViewModel(
     private fun getDataFromLocalSource(isRussian: Boolean) {
         liveDataToObserve.value = AppState.Loading
         Thread {
-            sleep(1000)
+            sleep(300)
             liveDataToObserve.postValue(
                 AppState.Success(
                     if (isRussian) repositoryImpl.getWeatherFromLocalStorageRus()
